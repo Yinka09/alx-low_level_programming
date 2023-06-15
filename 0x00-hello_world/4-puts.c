49ce9c35785fd95d724d04bd4b177e4bee59a312
Yinka09 <kadirimaranatha@yahoo.com>
2023-06-15 09:09:26 -0700

    C programming- Hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C-program that prints an output using the puts function
+ * Return: 0 Always
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A C-program that prints an output using the printf function
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
