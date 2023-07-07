c263f2bc825114a9d3b414ae128c846a6b43629b
Yinka09 <kadirimaranatha@yahoo.com>
2023-07-07 10:52:08 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -12,6 +12,6 @@ void _puts(char *str);
  */
 int main(void)
 {
-	_puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
