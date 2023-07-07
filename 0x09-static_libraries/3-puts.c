9873b62f1f3976a0ea36dcee7caa59af2c37210e
Yinka09 <kadirimaranatha@yahoo.com>
2023-07-07 10:39:46 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints output
+ * @str: input
+ */
+void _puts(char *str);
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,4 @@
+0x09. C - Static libraries
+
+0- Creates the static library called "libmy.a"
+1- Creates a script called "create_static_lib.sh" that creates a static library called "liball.a" from all the .c files that are in the current directory
