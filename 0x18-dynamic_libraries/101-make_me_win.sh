efc5346caace7eb23539d4dcb53296727b2c333c
Yinka09 <kadirimaranatha@yahoo.com>
2023-09-18 08:29:36 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-# Download the shared library to /tmp and specify the output file name
 wget -O /tmp/libgiga.so https://github.com/Yinka09/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-
-# Set LD_PRELOAD to the absolute path of the downloaded library
 export LD_PRELOAD=/tmp/libgiga.so
-
